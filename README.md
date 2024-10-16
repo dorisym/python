bb2f1ab1d33867af8c10edecceddb2231773d00d
dorisym <2439047839@qq.com>
2024-10-16 21:41:22 +0800

    修改git

---
+++ README.md
@@ -0,0 +1,14 @@
+# python
+我的自学py笔记
+
+git推送流程：本地->暂存区->远程仓库
+
+git status 查看修改了哪些文件
+
+git diff 确认修改内容，本地和远程仓库对比 文件修改的内容
+
+git add .  添加本地文件到暂存区
+
+git commit -m "xxxx" 为本次提交写一个备注
+
+git push  将暂存区的代码提交到远程仓库
--- testpkg123.py
+++ testpkg123.py
@@ -4,5 +4,5 @@ from datetime import datetime
 def dateFormat():
     now = datetime.now()
     formatted_date = now.strftime("%Y%m%d%H%M%S")
-    print("格式化的日期是22:", formatted_date)
+    print("格式化的日期是:", formatted_date)
     return formatted_date
